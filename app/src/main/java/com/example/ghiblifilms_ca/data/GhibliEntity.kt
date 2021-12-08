5969592cea2ba71497a3e84031b12b36e9b8bb32
Ben swan <swanben223@gmail.com>
2021-12-08 15:13:31 +0000

    Modified code

--- app/src/main/java/com/example/ghiblifilms_ca/data/GhibliEntity.kt
+++ app/src/main/java/com/example/ghiblifilms_ca/data/GhibliEntity.kt
@@ -1,14 +1,51 @@
 package com.example.ghiblifilms_ca.data
 
-import org.w3c.dom.Text
+import android.os.Parcel
+import android.os.Parcelable
 
 data class GhibliEntity(
-    var title: String,
-    var original_title: String,
-    var original_title_romanised: String,
-    var description: Text,
-    var director: String,
-    var producer: String,
-    var release_date: Int,
-    var running_time: Int
-)
+    var title: String?,
+    var original_title: String?,
+    var original_title_romanised: String?,
+    var description: String?,
+    var director: String?,
+    var producer: String?,
+    var release_date: String?,
+    var running_time: String?
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        TODO("description"),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(title)
+        parcel.writeString(original_title)
+        parcel.writeString(original_title_romanised)
+        parcel.writeString(director)
+        parcel.writeString(producer)
+        parcel.writeString(release_date)
+        parcel.writeString(running_time)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<GhibliEntity> {
+        override fun createFromParcel(parcel: Parcel): GhibliEntity {
+            return GhibliEntity(parcel)
+        }
+
+        override fun newArray(size: Int): Array<GhibliEntity?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
